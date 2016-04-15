cde61efeda04582899c7c0f6d2f17faacc6245f1
Mikhail Pustovalov <pustovalov.mike@gmail.com>
2016-04-16 01:46:31 +0300

    initial script

---
+++ ubuntu-postinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get p7zip-full mc htop iotop
+apt-get git mercurial subversion
+apt-get install ubuntu-restricted-extras
+apt-get install unzip unrar 
+apt-get install ruby ruby-dev
+apt-get install openssh-server
+apt-get install openjdk-8-jdk
+apt-get install ffmpeg gstreamer-tools
+apt-get install mplayer2
+apt-get install libzen0v5
+apt-get install libmediainfo0v5
+apt-get install vlc
+apt-get install golang-go
+#apt-get install transmission-cli transmission-common transmission-daemon
+apt-get install dstat
+apt-get install glances
