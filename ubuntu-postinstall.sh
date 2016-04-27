aaef769ed0d5086aa4e1b0cd3b63e28079e104b2
Mikhail Pustovalov <pustovalov.mike@gmail.com>
2016-04-27 17:59:37 +0300

    disable some specific steps

--- ubuntu-postinstall.sh
+++ ubuntu-postinstall.sh
@@ -12,12 +12,15 @@ apt-get install libzen0v5
 apt-get install libmediainfo0v5
 apt-get install vlc
 apt-get install golang-go
-#apt-get install transmission-cli transmission-common transmission-daemon
 apt-get install dstat
 apt-get install glances
 apt-get install vim-nox
-#swift language
-apt-get install git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config
-wget -q -O - https://swift.org/keys/all-keys.asc | gpg --import -
-#download swift from https://swift.org/download/#snapshots
 apt-get install bmon slurm tcptrack iftop nethogs
+
+#swift language (very buggy under ubuntu 15.10)
+#apt-get install git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config
+#wget -q -O - https://swift.org/keys/all-keys.asc | gpg --import -
+#download swift from https://swift.org/download/#snapshots
+
+#dlna server specific
+#apt-get install transmission-cli transmission-common transmission-daemon
