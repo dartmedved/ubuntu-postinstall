5ec231d2e25e8bb8d7d462dc04d9cf20881ae8a7
Mikhail Pustovalov <pustovalov.mike@gmail.com>
2016-05-11 23:51:44 +0300

    update  modules installations cript

--- node-postinstall.sh
+++ node-postinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-npm install -g yo grunt-cli bower gulp generator-jhipster
+npm install -g yo
+npm install -g grunt-cli 
+npm install -g bower 
+npm install -g gulp 
+npm install -g generator-jhipster
 
 #6.0.0 modules update
-npm install -g graceful-fs@^4.0.0 lodash@<3.0.0
+npm install -g graceful-fs@^4.0.0 
+npm install -g lodash@<3.0.0
