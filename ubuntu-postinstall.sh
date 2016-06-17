08921c655b1d9d91376a8f998e7d9f6cd849de43
Mikhail Pustovalov <pustovalov.mike@gmail.com>
2016-06-17 22:57:13 +0300

    Cleanup

--- ubuntu-postinstall.sh
+++ ubuntu-postinstall.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 apt -y install curl wget w3m
-apt-get -y install p7zip-full mc htop iotop
-apt-get -y install git mercurial subversion
-apt-get -y install ubuntu-restricted-extras
-apt-get -y install python3
-apt-get -y install unzip unrar 
-apt-get -y install ruby ruby-dev
-apt-get -y install openssh-server
-apt-get -y install openjdk-8-jdk
-apt-get -y install ffmpeg gstreamer-tools
-apt-get -y install mplayer2
-apt-get -y install libzen0v5
-apt-get -y install libmediainfo0v5
-apt-get -y install vlc
-apt-get -y install golang-go
-apt-get -y install dstat
-apt-get -y install glances
-apt-get -y install vim-nox
-apt-get -y install bmon slurm tcptrack iftop nethogs
-apt-get -y install taskwarrior
-apt-get -y install galculator
-apt-get -y install pwgen
+apt -y install p7zip-full mc htop iotop
+apt -y install git mercurial subversion
+apt -y install ubuntu-restricted-extras
+apt -y install python3
+apt -y install unzip unrar 
+apt -y install ruby ruby-dev
+apt -y install openssh-server
+apt -y install openjdk-8-jdk
+apt -y install ffmpeg gstreamer-tools
+apt -y install mplayer2
+apt -y install libzen0v5
+apt -y install libmediainfo0v5
+apt -y install vlc
+apt -y install golang-go
+apt -y install dstat
+apt -y install glances
+apt -y install vim-nox
+apt -y install bmon slurm tcptrack iftop nethogs
+apt -y install taskwarrior
+apt -y install galculator
+apt -y install pwgen
 apt -y install w3m
 
 #go tools
-apt-get -y install gocode
-apt-get -y install golang-golang-x-tools
+apt -y install gocode
+apt -y install golang-golang-x-tools
 
 #terminal emulators
-apt-get -y install terminator
-#apt-get install tilda
-apt-get -y install guake 
+apt -y install terminator
+#apt install tilda
+apt -y install guake 
 
 #swift language (very buggy under ubuntu 15.10)
-#apt-get install git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config
+#apt install git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config
 #wget -q -O - https://swift.org/keys/all-keys.asc | gpg --import -
 #download swift from https://swift.org/download/#snapshots
 
 #dlna server specific
-#apt-get -y install transmission-cli transmission-common transmission-daemon
+#apt -y install transmission-cli transmission-common transmission-daemon
+
+#cleanup 
+apt autoremove
