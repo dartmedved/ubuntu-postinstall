0db7ff82108a9ff9bfcaa05a05f9b19206932b30
Mike Pustovalov <pustovalov.mike@gmail.com>
2017-04-06 02:00:49 +0300

    + tig

--- ubuntu-postinstall.sh
+++ ubuntu-postinstall.sh
@@ -38,6 +38,7 @@ apt -y install guake
 apt -y install atom
 apt -y install code code-insiders
 apt -y install git-svn
+apt -y install tig
 
 #swift language (very buggy under ubuntu 15.10)
 #apt install git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config
