b7d7c1e558fbecf3674eb82d2b296b3703d6db02
Mike Pustovalov <pustovalov.mike@gmail.com>
2017-06-02 00:39:30 +0300

    SDK8 -> SDK8 zulu

--- sdkman/sdk-install.sh
+++ sdkman/sdk-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 8u131
+sdk install java 8u131-zulu
 sdk install maven 3.5.0
 sdk install groovy 2.4.9
 sdk install gradle 3.5
