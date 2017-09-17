6505666aca86027018b9c6bac75d1ca0f08742c4
Mike Pustovalov <pustovalov.mike@gmail.com>
2017-09-18 00:25:36 +0300

    cleanup

--- mate/upgrade-to-mate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt -y full-upgrade 
-apt -y autoremove 
-apt -y install --no-install-recommends ubuntu-mate-core
-apt -y install --no-install-recommends ubuntu-mate-desktop
-apt purge unity
-apt -y autoremove 
--- nodejs/node7-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-apt-get install -y nodejs
