4395763a419a486c710a6ea97cc26bf43b87beb4
Mike Pustovalov <pustovalov.mike@gmail.com>
2017-09-18 00:23:32 +0300

    remove versions

--- sdkman/sdk-install.sh
+++ sdkman/sdk-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 8u131-zulu
-sdk install maven 3.5.0
-sdk install groovy 2.4.9
-sdk install gradle 3.5
-sdk install kotlin 1.1.2-2
+sdk install java 
+sdk install maven 
+sdk install groovy 
+sdk install gradle 
+sdk install kotlin
 sdk upgrade
